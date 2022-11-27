df7352d844a61ec9d300531b705bae79c951e3ba
Azzam Uddin <uazzam69@gmail.com>
2022-11-27 12:41:44 +0530

    added async await to controllers

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,38 +1,43 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
+    try{
+        let post = await Post.findById(req.body.post);
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
-                if(err){console.log("Error in creating the comment"); return}
-
+            });   
                 post.comments.push(comment);
                 post.save();
 
                 res.redirect('/');
-            });
-        }
-    });
+        };
+    }catch(err){
+        console.log("Error", err);
+    }
+    
 };
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
+module.exports.destroy = async function(req, res){
+    try{
+        let comment = await Comment.findById(req.params.id);
+
         if(comment.user == req.user.id){
 
             let postId = comment.post;
 
             comment.remove();
 
-            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, function(err, post){
+            let post = await Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}});
                 return res.redirect('back');
-            });
         }else{
         return res.render('back');
         }
-    });
+    }catch(err){
+        console.log("Error", err);
+    }
+    
 }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,32 +2,29 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
 
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Connect | Home",
-    //         posts: posts
-    //     });
-
-    // });
-    
-    Post.find({}).populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users){
-            return res.render('home', {
-            title: "Connect | Home",
-            posts: posts,
-            all_users: users
-
+    try{
+        // populate the user of each post
+        let posts = await Post.find({}).
+        populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-    });
 
-    })
+        let users = await User.find({});
+
+        return res.render('home', {
+        title: "Connect | Home",
+        posts: posts,
+        all_users: users
+        });
+
+    }catch(err){
+        console.log("Error", err);
+    }
 }
+
